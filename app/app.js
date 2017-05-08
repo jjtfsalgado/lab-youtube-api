528984a87910eda1f92e281080c365224adf8041
José Salgado <jjtfsalgado@gmail.com>
2017-05-08 16:30:00 +0100

    First commit

---
+++ app/app.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+const API_KEY =	"AIzaSyDbnAP4IAcak1BqG7-zcWbBh3_B2Trdvy4";
+
+require('style!css!sass!applicationStyles');
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos:[],
+      term: 'surfboards',
+      selectedVideo: null
+    };
+    this.videoSearch(this.state.term);
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div className="container">
+        <SearchBar onSearchBar={ videoSearch }/>
+        <VideoDetail video={ this.state.selectedVideo }/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos }
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App/>
+  , document.getElementById('app'));
